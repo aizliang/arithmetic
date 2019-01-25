b34d28e213069d340c9e7cb14c34f666391d9c07
aizhaoliang <aizhaoliang1993@gmail.com>
2019-01-25 13:56:07 +0800

    字符检测

---
+++ String/StrStr.js
@@ -0,0 +1,14 @@
+/**
+ * strstr
+ * source: https://leetcode-cn.com/explore/featured/card/top-interview-questions-easy/5/strings/38/
+ */
+
+var strStr = function(haystack, needle) {
+	return haystack.indexOf(needle);
+};
+
+var haystack = 'hello';
+var needle = '';
+console.log(strStr(haystack, needle));
+
+
