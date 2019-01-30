c929496ba487017c972f5cd944aa3e8935a48378
aizhaoliang <aizhaoliang1993@gmail.com>
2019-01-30 13:48:51 +0800

    删除倒数第n个指针

--- List/DeleteNode.js
+++ List/DeleteNode.js
@@ -1,12 +1,31 @@
 /**
- * 删除节点
- * source: https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/6/linked-list/41/
+ * 删除链表的倒数第n个节点
+ * source: https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/6/linked-list/42/
  */
 
-var deleteNode = function(node) {
-	node.val = node.next.val;
-	node.next = node.next.next;
+var removeNthFromEnd = function(head, n) {
+	if (head == null || n==0) return head;
+
+    let fastNode = head;
+    let slowNode = head;
+    for (var i = 0; i < n; i++) {
+    	fastNode = fastNode.next;
+    }
+
+	//删除头节点
+  	if (fastNode == null) { 
+    	return head.next;
+    } 
+
+    while(fastNode.next != null) {
+    	fastNode = fastNode.next;
+    	slowNode = slowNode.next;
+    }
+  
+	slowNode.next = slowNode.next.next;
+	return head;
 };
+
 
 function ListNode(val) {
 	this.val = val;
@@ -21,9 +40,9 @@ function addNode(node) {
 var node0 = new ListNode(0);
 var node1 = new ListNode(1);
 var node2 = new ListNode(2);
+
 node0.addNode(node1);
 node1.addNode(node2);
 
 console.log(node0);
-deleteNode(node1)
-console.log(node0);
+console.log(removeNthFromEnd(node0, 3));
