427a0de1303c926d95595bfb9a6175cab420035e
aizliang <749994379@qq.com>
2019-09-14 11:52:14 +0800

    修改名称

--- 初级算法/JavaScript/List/DeleteNode.js
+++ 初级算法/JavaScript/List/DeleteNode.js
@@ -1,31 +1,12 @@
 /**
- * 删除链表的倒数第n个节点
- * source: https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/6/linked-list/42/
+ * 删除节点
+ * source: https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/6/linked-list/41/
  */
 
-var removeNthFromEnd = function(head, n) {
-	if (head == null || n==0) return head;
-
-    let fastNode = head;
-    let slowNode = head;
-    for (var i = 0; i < n; i++) {
-    	fastNode = fastNode.next;
-    }
-
-	//删除头节点
-  	if (fastNode == null) { 
-    	return head.next;
-    } 
-
-    while(fastNode.next != null) {
-    	fastNode = fastNode.next;
-    	slowNode = slowNode.next;
-    }
-  
-	slowNode.next = slowNode.next.next;
-	return head;
+var deleteNode = function(node) {
+	node.val = node.next.val;
+	node.next = node.next.next;
 };
-
 
 function ListNode(val) {
 	this.val = val;
@@ -40,9 +21,9 @@ function addNode(node) {
 var node0 = new ListNode(0);
 var node1 = new ListNode(1);
 var node2 = new ListNode(2);
-
 node0.addNode(node1);
 node1.addNode(node2);
 
 console.log(node0);
-console.log(removeNthFromEnd(node0, 3));
+deleteNode(node1)
+console.log(node0);
